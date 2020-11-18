e008f900c9dd1a0effe541ba57921196e3ce4f03
janusz10007 <48623077+janusz10007@users.noreply.github.com>
2020-11-18 23:49:24 +0100

    619spoj

---
+++ 619/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _619
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<t;i++)
+            {
+                string result = Convert(float.Parse(Console.ReadLine()));
+                string[] tab = result.Split('-');
+
+                result = "";
+
+                for (int j = tab.Length - 1; j >= 0; j--) ;
+                {
+                    if(tab[j][0] == 0)
+                    {
+                        result += tab[j][1];
+                    }
+                }
+            }
+        }
+
+        private static string Convert(float v)
+        {
+            return BitConverter.ToString(BitConverter.GetBytes(v));
+        }
+    }
+}
